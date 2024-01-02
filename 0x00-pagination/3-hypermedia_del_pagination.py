4d69877e4d128c4b2e96a96e21aa39627b58291e
Christopher Ndung'u <dev.kiratu@gmail.com>
2024-01-02 14:36:50 +0300

    use provided function prototype for get_hyper_index

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -5,7 +5,7 @@ Deletion-resilient hypermedia pagination
 
 import csv
 import math
-from typing import List, Union, Dict
+from typing import List, Dict
 
 
 class Server:
@@ -39,9 +39,7 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self,
-                        index: Union[int, None] = None,
-                        page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
         Implements deletion-resilient pagination
         """
