df9971753d5ecc14b3b6e80bcef4c78ab3279afd
Christopher Ndung'u <dev.kiratu@gmail.com>
2024-02-07 17:33:48 +0300

    add 6-job_creator.js

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,20 @@
+import { createQueue } from 'kue';
+
+const queue = createQueue();
+
+const jobData = {
+  phoneNumber: '123456',
+  message: 'hello kue'
+};
+
+const job = queue.createJob('push_notification_code', jobData).save((err) => {
+  if (!err) console.log(`Notification job created: ${job.id}`);
+});
+
+job.on('complete', (result) => {
+  console.log('Notification job completed');
+});
+
+job.on('failed', (errorMsg) => {
+  console.log('Notification job failed');
+});
