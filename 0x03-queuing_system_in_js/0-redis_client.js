8050622b99c18a00fdb0e5e970c798b35a1103b6
Christopher Ndung'u <dev.kiratu@gmail.com>
2024-02-07 15:36:04 +0300

    downgrade to redis 2.8.0

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -5,4 +5,3 @@ const client = createClient();
 client
   .on('ready', () => console.log('Redis client connected to the server'))
   .on('error', err => console.log('Redis client not connected to the server: ', err.message))
-  .connect();
